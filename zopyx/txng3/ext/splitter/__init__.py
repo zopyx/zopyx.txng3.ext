81f5f725482e8d68313d81f87055b6fe153cd950
Andreas Jung <yet@gmx.de>
2016-09-29 09:35:08 +0200

    added

---
+++ zopyx/txng3/ext/splitter/__init__.py
@@ -0,0 +1,11 @@
+# placeholder0
+
+
+class Splitter(object):
+
+    def __init__(self, casefolding=True, maxlen=None, singlechar=True, separator=u'.'):
+        self.casefolding = casefolding
+        self.maxlen = maxlen
+        self.singlechar = singlechar
+        self.separator = separator
+
